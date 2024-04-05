6aa56cac4de6d5eb2a40bd7792eef8617f69e09d
ParubokMotorniy <oles.yarish@gmail.com>
2024-04-05 18:52:58 +0300

    Я обожнюю веб і скайрім...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import MyThree from './components/three/Three.js'
+import SolarSystem from "./components/three/WelcomePage.js"
 
 function App() {
   return (
     <div className="App">
-      <MyThree />
+      {/* <MyThree /> */}
+      <SolarSystem/>
     </div>
   );
 }
