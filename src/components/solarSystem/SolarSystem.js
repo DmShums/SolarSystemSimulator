cf7688ff848ae39150fd2ec4b2df336eeb0c4310
ParubokMotorniy <oles.yarish@gmail.com>
2024-04-07 05:07:03 +0300

    Added solar system functionality

--- src/components/solarSystem/SolarSystem.js
+++ src/components/solarSystem/SolarSystem.js
@@ -1,14 +1,72 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { RotationQuaternion } from "../../lib/QuaternionLibrary";
-
 import sky1 from "../../imgs/sky1.png";
 import sky2 from "../../imgs/sky2.png";
 import sky3 from "../../imgs/sky3.png";
 import sky4 from "../../imgs/sky4.png";
+import mercuryTex from "../../imgs/mercury.jpg";
+import venusTex from "../../imgs/venus.jpg";
+import sunTex from "../../imgs/sun.jpg";
 
 const SolarSystem = () => {
+  const [children, setChildren] = useState([]);
+
+  function addPlanet(newPlanet){
+    setChildren([...children, newPlanet]);
+  };
+
+  function removePlanet (planetID){
+    setChildren(children.filter((child) => child["ID"] !== planetID));
+  };
+
+  function getNewPlanetTransform(planetConfig, time){
+    const x = planetConfig["a"] * Math.cos(time);
+    const y = planetConfig["b"] * Math.sin(time);
+    const z = planetConfig["c"] * Math.sin(time);
+
+    const rotation = new RotationQuaternion(0,0,1,planetConfig["d"] * Math.PI);
+
+    return [x,y,z,rotation];
+  }
+
+  function createPlanet(size, texture, ring){
+    const textureload = new THREE.TextureLoader();
+
+    const geometry = new THREE.SphereGeometry(size, 45, 35);
+    const material = new THREE.MeshStandardMaterial({
+      map:textureload.load(texture)
+    });
+
+    const planet = new THREE.Mesh(geometry, material);
+    sceneRef.current.add(planet);
+    planet.position.x = 0;
+    planet.position.y = 0;
+    planet.position.z = 0;
+    planet.needsUpdate = true;
+
+    if(ring)
+    {
+      const RingGeo = new THREE.RingGeometry(
+        ring.innerRadius,
+        ring.outerRadius, 30
+      );
+      const RingMat = new THREE.MeshStandardMaterial({
+        map:textureload.load(ring.texture),
+        side : THREE.DoubleSide
+      });
+      const Ring = new THREE.Mesh(RingGeo, RingMat);
+      planet.add(Ring);
+
+      ring.position.x = 0;
+      ring.position.y = 0;
+      ring.position.z = 0;
+      Ring.rotation.x = -0.5 *Math.PI;
+    }
+    return planet;
+  }
+
   // Refs for scene, camera, and renderer
   const containerRef = useRef(null);
   const sceneRef = useRef(null);
@@ -35,7 +93,7 @@ const SolarSystem = () => {
     containerRef.current.appendChild(renderer.domElement);
     ///////Init stuff
     const orbit = new OrbitControls(camera, renderer.domElement);
-
+    orbit.enablePan = false;
     camera.position.set(-90, 140, 140);
     orbit.update();
 
@@ -49,20 +107,44 @@ const SolarSystem = () => {
       sky4,
     ]);
 
-    const ambientLight = new THREE.AmbientLight(0x333333);
+    const ambientLight = new THREE.AmbientLight(0x935353);
     scene.add(ambientLight);
-    /////////////////
 
+    const sun = {"planet": createPlanet(60, sunTex), planetConfig:{"a":0, "b":0, "c":0, "d":0.15}, "ID":0};
+    const mercury = {"planet": createPlanet(25, mercuryTex), planetConfig:{"a":500, "b":250, "c":300, "d":0.05}, "ID":1};
+    const venus = {"planet": createPlanet(30, venusTex), planetConfig:{"a":600, "b":350, "c":300, "d":0.1}, "ID":2};
+    
+    addPlanet(sun);
+    addPlanet(mercury);
+    addPlanet(venus);
+
+    /////////////////
     const animate = () => {
-      requestAnimationFrame(animate);
+      const curTime = performance.now()/1000;
+      for(let planet of children)
+      {
+        const newTransform = getNewPlanetTransform(planet.planetConfig, curTime);
+
+        console.log(`New: X:${newTransform[0]};Y:${newTransform[1]};Z:${newTransform[2]}`);
+
+        console.log(`Before: X:${planet.planet.position.x};Y:${planet.planet.position.y};Z:${planet.planet.position.z}`);
+        // planet.planet.position.x = newTransform[0];
+        // planet.planet.position.y = newTransform[1];
+        // planet.planet.position.z = newTransform[2];
+
+        planet.planet.position.set(newTransform[0], newTransform[1], newTransform[2]);
+        console.log(`After: X:${planet.planet.position.x};Y:${planet.planet.position.y};Z:${planet.planet.position.z}`);
+
+        newTransform[3].ApplyToThreeObject(planet["planet"]);
+      }
 
       renderer.render(scene, camera);
+      requestAnimationFrame(animate);
     };
-
     animate();
 
     return () => {
-      // Cleanup function
+      cancelAnimationFrame(animate);
       renderer.dispose();
 
       scene.traverse((obj) => {
@@ -77,5 +159,4 @@ const SolarSystem = () => {
 
   return <div ref={containerRef} />;
 };
-
 export default SolarSystem;
---
+++ src/components/solarSystem/SystemPlanet.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+const SystemPlanet = ({params}) => {
+    const containerRef = useRef(null);
+
+    getCurrentCoordinates()
+    {
+        const time = performance.now()/1000;
+
+        
+    }
+
+    useEffect(() => {
+        const geometry = new THREE.SphereGeometry(params.planetSize, 25, 20);
+        const material = new THREE.MeshStandardMaterial({
+        map:textureload.load(params.texturePath)
+        });
+
+        const planet = new THREE.Mesh(geometry, material);
+        const planetWrapper = new THREE.Object3D;
+        planetWrapper.add(planet);
+        params.scene.add(planetWrapper);
+        planet.position.x = params.position;
+
+        if(params.ring){
+            const RingGeo = new THREE.RingGeometry(
+                params.ring.innerRadius,
+                params.ring.outerRadius, 30
+            );
+            const RingMat = new THREE.MeshStandardMaterial({
+                map:textureload.load(params.ring.texturePath),
+                side : THREE.DoubleSide
+            });
+            const Ring = new THREE.Mesh(RingGeo, RingMat);
+            planetWrapper.add(Ring);
+
+            Ring.position.x = params.position;
+            Ring.rotation.x = -0.5 *Math.PI;
+        }
+    }, []);
+
+    return <div ref={containerRef} />;
+}
+
+export default SystemPlanet;
