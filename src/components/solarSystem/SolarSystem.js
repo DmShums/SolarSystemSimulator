63a5d828203686321101eaf003e1648995257cb0
ParubokMotorniy <oles.yarish@gmail.com>
2024-04-15 16:02:38 +0300

    removed floppa :(

--- src/components/solarSystem/SolarSystem.js
+++ src/components/solarSystem/SolarSystem.js
@@ -3,12 +3,19 @@ import * as THREE from "three";
 import { InteractionManager } from "three.interactive";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { RotationQuaternion } from "../../lib/QuaternionLibrary";
-import sky1 from "../../imgs/floppface.jpeg";
-import sky2 from "../../imgs/floppaass.jpeg";
-import sky3 from "../../imgs/floppatop.jpeg";
-import sky4 from "../../imgs/floppaleft.jpeg";
-import sky5 from "../../imgs/flopparight.jpeg";
-import sky6 from "../../imgs/floppabottom.jpeg";
+// import sky1 from "../../imgs/floppface.jpeg";
+// import sky2 from "../../imgs/floppaass.jpeg";
+// import sky3 from "../../imgs/floppatop.jpeg";
+// import sky4 from "../../imgs/floppaleft.jpeg";
+// import sky5 from "../../imgs/flopparight.jpeg";
+// import sky6 from "../../imgs/floppabottom.jpeg";
+
+import sky1 from "../../imgs/box1.jpg";
+import sky2 from "../../imgs/box2.jpg";
+import sky3 from "../../imgs/box3.jpg";
+import sky4 from "../../imgs/box4.jpg";
+import sky5 from "../../imgs/box5.jpg";
+import sky6 from "../../imgs/box6.jpg";
 
 import mercuryTex from "../../imgs/mercury.jpg";
 import venusTex from "../../imgs/venus.jpg";
